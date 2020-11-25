b39051b6bf7d5dad2e0f551f4d2f521bd5151b66
calouvts <pascalevetois@gmail.com>
2020-11-25 20:59:04 +0100

    modification

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,12 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+/**
+ * @Route("/programs", name="program_")
+ */
 class ProgramController extends AbstractController
 {
     /**
 
-     * @Route("/programs/", name="program_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
